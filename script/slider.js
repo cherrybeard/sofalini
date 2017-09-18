ca468ee7e10afc686b13ecd6dc6663fcc23f0240
Nalopka <Nalopka@gmail.com>
2017-09-18 16:56:17 +0700

    first push

---
+++ script/slider.js
@@ -0,0 +1,11 @@
+$(function() {
+	$(".slider").slick({
+		// accessibility: true,
+		infinite: false,
+		variableWidth: true,
+		initialSlide: 0,
+		prevArrow: '<button type="button" data-role="none" class="slick-prev"></button>',
+		nextArrow: '<button type="button" data-role="none" class="slick-next"></button>',
+		// useCSS: true
+	});
+});
