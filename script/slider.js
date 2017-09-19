9a84d20eedefe60ce4c29df1b7bc83fcb19f5ef5
Nalopka <Nalopka@gmail.com>
2017-09-19 14:17:42 +0700

    wrk

--- script/slider.js
+++ script/slider.js
@@ -1,11 +1,30 @@
 $(function() {
+
 	$(".slider").slick({
-		// accessibility: true,
-		infinite: false,
+		infinite: true,
 		variableWidth: true,
 		initialSlide: 0,
 		prevArrow: '<button type="button" data-role="none" class="slick-prev"></button>',
 		nextArrow: '<button type="button" data-role="none" class="slick-next"></button>',
-		// useCSS: true
+
+
 	});
+
+// 	$('.slider').on('afterChange', function (event, slick, currentSlide) {
+
+// 		if(currentSlide == 0) {
+//             $('.slick-prev').addClass('hidden');
+//         }
+//         else {
+//             $('.slick-prev').removeClass('hidden');
+//         }  
+
+//         if(currentSlide === 2) {
+//             $('.slick-next').addClass('hidden');
+//         }
+//         else {
+//             $('.slick-next').removeClass('hidden');
+//         }
+
+// });
 });
