7292599bca5d7fd7ff04486eb0896f9ede0638c0
Steven Gusev <stephen.gusev@gmail.com>
2017-09-22 06:45:00 +0900

    Images for sliders and mail form

---
+++ mail.php
@@ -0,0 +1,24 @@
+<?
+	if (count($_POST)!=0) {
+		$msg = 'Новая заявка с лендинга Sofalini #1<br>';
+		if (isset($_POST['phone'])) {
+			$msg .= 'Телефон: '.$_POST['phone'].'<br>';
+		}
+		if (isset($_POST['email'])) {
+			$msg .= 'E-mail: '.$_POST['email'].'<br>';
+		}
+		if (isset($_POST['message'])) {
+			$msg .= 'Хочу: '.$_POST['message'].'<br>';
+		}
+		$title = 'Заявка с лендинга Sofalini #1';
+		if (!empty($_POST['msg'])) $msg .= '<br>'.$_POST['msg'];
+    $from = 'pavlova@blackstonekeeping.com';
+		$header = 'MIME-Version: 1.0' . "\n" .
+			'Content-type: text/html; charset=UTF-8'. "\n" .
+			'From: ColorEvent <' . $from . ">\n";
+    $to = 'pavlova@blackstonekeeping.com';
+    if (mail($to, $title, $msg, $header))
+    	die('1');
+	}
+	if (count($_POST)!=0) die('0');
+?>
--- script/forms.js
+++ script/forms.js
@@ -5,4 +5,40 @@ $(function() {
 	$('.contacts').on('click', '.callback', function(){
 		$(this).closest('.contacts').addClass('contacts-opened');
 	});
+
+	checkInputs = function($inputs){
+		$inputs.each(function(){
+			if (!$(this).val()) {$(this).closest('.text-input').addClass('error'); }
+		});
+	};
+
+	$('[data-form]').on('submit', function(e){
+		$this = $(this);
+		e.preventDefault();
+
+		checkInputs($this.find('input[type=text]'));
+		checkInputs($this.find('input[type=email]'));
+		checkInputs($this.find('textarea'));
+
+		if (!$this.find('.text-input.error').length){
+			var ajaxData = {
+				url: 'index.php',
+				type: $this.attr('method'),
+				data: $this.serialize()
+			};
+			$this.find('button[type=submit]').prop('disabled', true);
+			$this.find('.text-input').addClass('disabled');
+			$.ajax(ajaxData).done(function(req) {
+				$this.find('button[type=submit]').prop('disabled', false).addClass('success').text('Отправлено!');
+			});
+		}
+	});
+
+	$('[data-form]').on('focus', 'input', function(){
+		$(this).closest('.text-input').removeClass('error');
+	});
+
+	$('[data-form]').on('focus', 'textarea', function(){
+		$(this).closest('.text-input').removeClass('error');
+	});
 });
