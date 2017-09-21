3cab9d60c81f44e37ea70fd5db21306c8b741fa8
Steven Gusev <stephen.gusev@gmail.com>
2017-09-22 04:03:50 +0900

    Sliders improvements

--- script/slider.js
+++ script/slider.js
@@ -1,80 +1,23 @@
-
 $(function() {
-    var swiperHeader = new Swiper ('.header', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        paginationClickable: true,
-        effect: 'fade',
-        autoplay: 5000,
-        loop: true
-        // grabCursor: true,
-        // slidesPerView: "auto"
-    });
-    var swiperTransform = new Swiper ('.transformers', {
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        grabCursor: true,
-        slidesPerView: "auto"
-    });   
-    var swiperSingle = new Swiper ('.single', {
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        grabCursor: true,
-        slidesPerView: "auto"
-    }); 
-      
+  var swiperHeader = new Swiper ('.header', {
+      pagination: '.swiper-pagination',
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      paginationClickable: true,
+      effect: 'fade',
+      autoplay: 5000,
+      loop: true
+  });
+
+	DEFAULT_SLIDER = {
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    grabCursor: true,
+    slidesPerView: "auto"
+  };
+  var swiperTransform = new Swiper ('.transformers .swiper-container', DEFAULT_SLIDER);
+  var swiperEco 			= new Swiper ('.eco .swiper-container', DEFAULT_SLIDER);
+  var swiperSingle 		= new Swiper ('.single .swiper-container', DEFAULT_SLIDER);
+  var swiperPremium 	= new Swiper ('.premium .swiper-container', DEFAULT_SLIDER);
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 	$(".slider").slick({
-// 		infinite: true,
-// 		variableWidth: true,
-// 		initialSlide: 0,
-// 		prevArrow: '<button type="button" data-role="none" class="slick-prev"></button>',
-// 		nextArrow: '<button type="button" data-role="none" class="slick-next"></button>',
-
-
-// 	});
-
-	// $('.slider').on('afterChange', function (event, slick, currentSlide) {
-
-	// 	if(currentSlide == 0) {
- //            $('.slick-prev').addClass('hidden');
- //        }
- //        else {
- //            $('.slick-prev').removeClass('hidden');
- //        }  
-
- //        if(currentSlide === 4) {
- //            $('.slick-next').addClass('hidden');
- //        }
- //        else {
- //            $('.slick-next').removeClass('hidden');
- //        }
-
-	// });
