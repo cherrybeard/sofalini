f5b0e43330d4454207b2988cbb7ecb940aec8f50
Steven Gusev <stephen.gusev@gmail.com>
2017-10-13 19:30:21 +0700

    more ga

--- index.php
+++ index.php
@@ -419,6 +419,11 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.2/js/swiper.jquery.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="js/main.js"></script><script>
+  (function (w,i,d,g,e,t,s) {w[d] = w[d]||[];t= i.createElement(g);
+    t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t, s);
+  })(window, document, '_gscq','script','//widgets.getsitecontrol.com/106898/script.js');
+</script>
+
   </body>
 </html>
